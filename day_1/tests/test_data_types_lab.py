963afd92be28d59295661a697634199bd220bff2
Dominic Motuka Monyoncho <dominic.motuka@gmail.com>
2017-04-04 13:34:49 +0300

    Add unit tests for data types lab

---
+++ day_1/tests/test_data_types_lab.py
@@ -0,0 +1,22 @@
+class DataTypeTestCase(TestCase):
+
+  def test_none_type(self):
+    self.assertEqual('no value', data_type(None))
+
+  def test_array_type(self):
+    self.assertEqual(3, data_type([1, 2, 3]))
+
+  def test_small_array_type(self):
+    self.assertEqual(None, data_type([1, 2]))
+
+  def test_small_booleans_type(self):
+    self.assertEqual(True, data_type(True))
+
+  def test_small_integer_type(self):
+    self.assertEqual('less than 100', data_type(3))
+
+  def test_large_integer_type(self):
+    self.assertEqual('more than 100', data_type(200))
+  
+  def test_str_type(self):
+    self.assertEqual(6, data_type('andela'))
