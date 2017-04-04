16d3ba8e516129a3a46c3913b650a0237a35a2e9
Dominic Motuka Monyoncho <dominic.motuka@gmail.com>
2017-04-05 00:38:51 +0300

    Modify primeSieve to handle more cases

--- day_1/primeSieve.py
+++ day_1/primeSieve.py
@@ -13,24 +13,41 @@ def isPrime(num): #Returns true if num is a prime number otherwise False
 '''In Mathematics the sieve of Eratosthenes is an algorithm  used to algorithm 
 for finding all prime numbers up to any given limit
 '''
-def primeSieve(n): # returns a list of all prime numbers with  n as sieve size 
+def primeSieve(n):
+	if type(n) != int:
+		return 'TypeError'
+	elif n < 0:
+		return 'ValueError'
+	else:
+		primes = [] # Initialize array to hold prime numbers
+		for i in range(n):
+			if isPrime(i):
+				primes.append(i)
+		return primes
+print(primeSieve(0))
+print(primeSieve(-100))
+print(primeSieve('rtr'))
+print(primeSieve(9))
+
+
+# def primeSieve(n): # returns a list of all prime numbers with  n as sieve size 
  
- 	sieve = [True]* n
- 	sieve[0] = False # Any number less than 2 is not prime
- 	sieve[1] = False
+#  	sieve = [True]* n
+#  	sieve[0] = False # Any number less than 2 is not prime
+#  	sieve[1] = False
 
- 	for i in range(2,int(math.sqrt(n))+1): # converts list indices from tuples to integers
- 		reference = i*2 #starts at the first multiple of i which is i*2
+#  	for i in range(2,int(math.sqrt(n))+1): # converts list indices from tuples to integers
+#  		reference = i*2 #starts at the first multiple of i which is i*2
 
- 		while reference < n:
- 			sieve[reference]=False #set the reference index in the sieve list to False
- 			reference += i #increments to the next number multiple of i
+#  		while reference < n:
+#  			sieve[reference]=False #set the reference index in the sieve list to False
+#  			reference += i #increments to the next number multiple of i
 
- 		'''After the while is loop is through, the sieve list will contain
- 		True for each index that is a prime number '''
+#  		'''After the while is loop is through, the sieve list will contain
+#  		True for each index that is a prime number '''
 
- 	primes = [] # Initialize array to hold prime numbers
- 	for i in range(n):
- 		if sieve[i] == True:
- 			primes.append(i)
- 	return primes
+#  	primes = [] # Initialize array to hold prime numbers
+#  	for i in range(n):
+#  		if sieve[i] == True:
+#  			primes.append(i)
+#  	return primes
