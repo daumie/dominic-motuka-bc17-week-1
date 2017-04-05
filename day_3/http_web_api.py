81ceee4bda88ae43202f84d0307a7222764a7f77
Dominic Motuka Monyoncho <dominic.motuka@gmail.com>
2017-04-05 14:37:43 +0300

    A simple application too determine position of International Space station

---
+++ day_3/http_web_api.py
@@ -0,0 +1,27 @@
+import http.client
+import requests
+from pprint import pprint
+import json
+
+conn = http.client.HTTPConnection("www.open-notify.com")
+conn.request("HEAD", "/")
+res = conn.getresponse()
+response = requests.get("http://api.open-notify.org/iss-now.json")
+response.json()
+
+if res.status == 500:
+    print("INTERNAL SERVER ERROR")
+elif res.status == 404:
+    print("THE PAGE CANNOT BE FOUND")
+elif res.status == 403:
+    print("UNAUTHORIZED ACCESS")
+elif res.status == 400:
+    print("BAD REQUEST")
+elif res.status == 200:
+    print("status OK")
+elif res.status == 301:
+    print("OK , WELCOME TO THE INTERNATIONAL SPACE STATION ")
+    print("Current posisition of the International Space Station Satelite is :\n")
+    pprint(response.json())
+else:
+    print("unable to ACCESS data")
