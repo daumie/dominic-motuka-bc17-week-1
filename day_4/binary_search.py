dbc03b634ba99454c6cce848ba4433a5c32cad15
Dominic Motuka Monyoncho <dominic.motuka@gmail.com>
2017-04-05 21:12:44 +0300

    Quickly find values using binary search

--- day_4/binary_search.py
+++ day_4/binary_search.py
@@ -1,19 +1,19 @@
 def binary_search(given_list, item):
-    first_item = 0
-    last_item = len(given_list) - 1
-    found = False
 
-    while first_item <= last_item and not found:
-        midpoint = (first_item + last_item) // 2
-        if given_list[midpoint] == item:
-            found = True
-        else:
-            if item < given_list[midpoint]:
-                last_item = midpoint - 1
-            else:
-                first_item = midpoint - 1
-                return found
+    if len(given_list) == 0:
+    	return False
+    else:
+    	midpoint = len(given_list) // 2
+
+    if given_list[midpoint] == item:
+    	return True
+    else:
+    	if item < given_list[midpoint]:
+    		return binary_search(given_list[:midpoint], item)
+    	else:
+    		return binary_search(given_list[midpoint + 1:],item)
 test_list = [21,27,32,33,42,47,58,69,70,77,79]
 print(binary_search(test_list,3))
-print(binary_search(test_list,30))
+print(binary_search(test_list,69))
+    		
 
