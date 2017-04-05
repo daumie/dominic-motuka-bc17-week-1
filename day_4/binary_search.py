8350b37a16b0d98308d7277684e872c3015c901f
Dominic Motuka Monyoncho <dominic.motuka@gmail.com>
2017-04-06 01:22:54 +0300

    Sort given list for binary search to work on it
    Binary takes advantage ordered list for comparisons
    Added sort function to order the given list.

--- day_4/binary_search.py
+++ day_4/binary_search.py
@@ -1,17 +1,19 @@
 def binary_search(given_list, item):
+    
+    sorted_given_list = sorted(given_list)# sort the given list
 
-    if len(given_list) == 0:
+    if len(sorted_given_list) == 0:
     	return False
     else:
-    	midpoint = len(given_list) // 2
+    	midpoint = len(sorted_given_list) // 2
 
-    if given_list[midpoint] == item:
+    if sorted_given_list[midpoint] == item:
     	return True
     else:
-    	if item < given_list[midpoint]:
-    		return binary_search(given_list[:midpoint], item)
+    	if item < sorted_given_list[midpoint]:
+    		return binary_search(sorted_given_list[:midpoint], item)
     	else:
-    		return binary_search(given_list[midpoint + 1:],item)
+    		return binary_search(sorted_given_list[midpoint + 1:],item)
 # test_list = [21,27,32,33,42,47,58,69,70,77,79]
 # print(binary_search(test_list,3))
 # print(binary_search(test_list,69))
