c90c78ab7e84c4ae790d1ea6828ee209b9a2b3eb
Dominic Motuka Monyoncho <dominic.motuka@gmail.com>
2017-04-27 11:17:16 +0300

    Add quiz for protor OOP and model

---
+++ labs/model/banklab.py
@@ -0,0 +1,48 @@
+class BankAccount(object):
+    def withdraw(self):
+        pass
+
+    def deposit(self):
+        pass
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self):
+        self.balance = 500
+
+    def deposit(self, amount):
+        if amount < 0:
+            return 'Invalid deposit amount'
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount < 0:
+            return 'Invalid withdraw amount'
+        elif amount > self.balance:
+            return 'Cannot withdraw beyond the current account balance'
+        elif (self.balance - amount) < 500:
+            return 'Cannot withdraw beyond the minimum account balance'
+        else:
+            self.balance -= amount
+            return self.balance
+
+
+class CurrentAccount(BankAccount):
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, amount):
+        if amount < 0:
+            return 'Invalid deposit amount'
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount < 0:
+            return 'Invalid withdraw amount'
+        elif amount > self.balance:
+            return 'Cannot withdraw beyond the current account balance'
+        else:
+            self.balance -= amount
+            return self.balance
