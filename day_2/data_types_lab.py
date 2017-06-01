c227ea0e1914de65ee9e59b439c943872076153f
Dominic Motuka <dominic.motuka@gmail.com>
2017-06-01 18:21:07 +0300

    [Fix] use accepted comparison (pep8)

--- day_2/data_types_lab.py
+++ day_2/data_types_lab.py
@@ -1,7 +1,7 @@
 """Docstring"""
 def data_type(data):
     """returns type of data"""
-    if type(data) == type(None):
+    if data is None:
         return 'no value'
     elif isinstance(data, str):
         return len(data)
