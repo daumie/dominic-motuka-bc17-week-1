7b8c802a1f459cf91cbed622f5b52df189d03abd
Dominic Motuka <dominic.motuka@gmail.com>
2017-06-01 18:18:27 +0300

    [Fix] Correctly format docstrings

--- day_1/tests/test_primeSieve.py
+++ day_1/tests/test_primeSieve.py
@@ -4,22 +4,23 @@ from day_1.primesieve import primesieve
 
 
 class PrimeGenerator(unittest.TestCase):
-    """Contains tests for the sieve of eratosthenes"""
+    """Contains tests for the sieve of eratosthenes 
+    for generating prime numbers"""
 
     def test_empty_list_for_1(self):
-        ''' Test if it returns an empty list when given input 1'''
+        """Test if it returns an empty list when given input 1"""
         self.assertEqual(primesieve(1), [])
     def test_empty_list_for_0(self):
-        '''Test if it returns an empty list with 0 as input'''
+        """Test if it returns an empty list with 0 as input"""
         self.assertEqual(primesieve(0), [])
     def test_for_str_type_error(self):
-        '''Test for TypeError for string input'''
+        """Test for TypeError for string input"""
         self.assertEqual(primesieve('random str'), 'TypeError')
     def test_for_negative_value_error(self):
-        '''Test for ValueError for negative number input'''
+        """Test for ValueError for negative number input"""
         self.assertEqual(primesieve(-100), 'ValueError')
     def test_correct_values(self):
-        ''' Test for correct values'''
+        """Test for correct values"""
         self.assertEqual(primesieve(30), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29])
 
 
